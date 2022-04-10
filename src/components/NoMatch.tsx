546eebc704ad856a9974345ad6bce1461cac54cd
karolwk <krl@o2.pl>
2022-04-10 12:34:05 +0200

    fixed layout

--- src/components/NoMatch.tsx
+++ src/components/NoMatch.tsx
@@ -1,12 +1,17 @@
-import { FaBeer } from 'react-icons/fa';
-
+import { FaRegFrown } from 'react-icons/fa';
+import { Link } from 'react-router-dom';
 const NoMatch: React.FC = () => {
   return (
     <main>
       <div className="page-404">
         <h1>
-          404 - Page not found <FaBeer />
+          404 - Page not found <FaRegFrown />
         </h1>
+        <Link to="/">
+          <button className="btn-standard">
+            <span>&larr;</span> Back
+          </button>
+        </Link>
       </div>
     </main>
   );
